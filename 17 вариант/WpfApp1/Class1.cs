a973cfb4bff77db06ace3ae61e142876b270d3ab
Avier228 <84218777+Avier228@users.noreply.github.com>
2021-05-18 09:38:18 +0300

    добавление схемы игры
    Добавление при каких схемах кто побеждает, так же вывод имя победителя. Но сама игра пока еще не готова.

---
+++ 17 вариант/WpfApp1/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Player1
+    {
+        public static string Name;
+        public static bool Stone;
+        public static bool Scissors;
+        public static bool Paper;
+        public static bool xod;
+    }
+    class Player2
+    {
+        public static string Name;
+        public static bool Stone;
+        public static bool Scissors;
+        public static bool Paper;
+        public static bool xod;
+    }
+}
--- 17 вариант/WpfApp1/MainWindow.xaml.cs
+++ 17 вариант/WpfApp1/MainWindow.xaml.cs
@@ -21,5 +21,83 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+        private void ButtonPlay_Click(object sender, RoutedEventArgs e)
+        {
+            Player1.Name = null;
+            Player1.Paper = false;
+            Player1.Scissors = false;
+            Player1.Stone = false;
+            Player1.xod = false;
+
+            Player2.Name = null;
+            Player2.Paper = false;
+            Player2.Scissors = false;
+            Player2.Stone = false;
+            Player2.xod = false;
+            this.Visibility = Visibility.Collapsed;
+            /// Window1 Win1 = new Window1();
+            /// Win1.ShowDialog();
+        }
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void winnertext(object sender, EventArgs e)
+        {
+            if (Player1.Paper)
+            {
+                if (Player2.Paper)
+                {
+                    LabelWinner.Content = "ничья между" + " " + $"{Player1.Name}" + " " + "и" + " " + $"{Player2.Name}";
+                }
+
+                if (Player2.Scissors)
+                {
+                    LabelWinner.Content = $"{Player2.Name} Выйграл";
+                }
+
+                if (Player2.Stone)
+                {
+                    LabelWinner.Content = $"{Player1.Name} Выйграл";
+                }
+            }
+
+            if (Player1.Scissors)
+            {
+                if (Player2.Paper)
+                {
+                    LabelWinner.Content = $"{Player1.Name} Выйграл";
+                }
+
+                if (Player2.Scissors)
+                {
+                    LabelWinner.Content = "ничья между" + " " + $"{Player1.Name}" + " " + "и" + " " + $"{Player2.Name}";
+                }
+
+                if (Player2.Stone)
+                {
+                    LabelWinner.Content = $"{Player2.Name} Выйграл";
+                }
+            }
+
+            if (Player1.Stone)
+            {
+                if (Player2.Paper)
+                {
+                    LabelWinner.Content = $"{Player2.Name} Выйграл";
+                }
+
+                if (Player2.Scissors)
+                {
+                    LabelWinner.Content = $"{Player1.Name} Выйграл";
+                }
+
+                if (Player2.Stone)
+                {
+                    LabelWinner.Content = "ничья между" + " " + $"{Player1.Name}" + " " + "и" + " " + $"{Player2.Name}";
+                }
+            }
+        }
     }
 }
