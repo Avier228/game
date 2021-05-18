47ecc5317ecda90d5cf1f0f4875604170753f2d8
Avier228 <84218777+Avier228@users.noreply.github.com>
2021-05-18 09:49:55 +0300

    Итоговая версия продукта
    Полностью рабочая версия игры, в которой можно изменить интерфейс или сделать пару возможностей, но сама игра работает.

--- 17 вариант/WpfApp1/Window1.xaml.cs
+++ 17 вариант/WpfApp1/Window1.xaml.cs
@@ -14,14 +14,85 @@ using System.Windows.Shapes;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Логика взаимодействия для Window1.xaml
-    /// </summary>
     public partial class Window1 : Window
     {
         public Window1()
         {
             InitializeComponent();
+            if (Player1.xod) LabelSelectItem.Content = "Выбирайте";
+
+            else LabelSelectItem.Content = "Выбирайте";
+        }
+        private void Window_Closed(object sender, EventArgs e)
+        {
+
+            if (!Player2.xod)
+            {
+
+                Window1 Win0 = new Window1();
+                Win0.ShowDialog();
+            }
+            else
+            {
+
+                MainWindow MainWin2 = new MainWindow();
+                MainWin2.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void ButtonA_Click(object sender, RoutedEventArgs e)
+        {
+            if (Player1.xod)
+            {
+
+                Player2.Stone = true;
+                Player2.xod = true;
+                Player2.Name = TextBoxNamePlayer.Text;
+            }
+            else
+            {
+
+                Player1.Stone = true;
+                Player1.xod = true;
+                Player1.Name = TextBoxNamePlayer.Text;
+            }
+            this.Close();
+        }
+        private void ButtonB_Click(object sender, RoutedEventArgs e)
+        {
+            if (Player1.xod)
+            {
+
+                Player2.Scissors = true;
+                Player2.xod = true;
+                Player2.Name = TextBoxNamePlayer.Text;
+            }
+            else
+            {
+
+                Player1.Scissors = true;
+                Player1.xod = true;
+                Player1.Name = TextBoxNamePlayer.Text;
+            }
+            this.Close();
+        }
+        private void ButtonC_Click(object sender, RoutedEventArgs e)
+        {
+            if (Player1.xod)
+            {
+
+                Player2.Paper = true;
+                Player2.xod = true;
+                Player2.Name = TextBoxNamePlayer.Text;
+            }
+            else
+            {
+
+                Player1.Paper = true;
+                Player1.xod = true;
+                Player1.Name = TextBoxNamePlayer.Text;
+            }
+            this.Close();
         }
 
     }
